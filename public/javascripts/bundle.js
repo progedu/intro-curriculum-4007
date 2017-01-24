0745346e8d8f01b6342046bfc4d1b20b22f6e6c3
kanijpn <masahiro@okatani.com>
2017-01-24 12:48:17 +0000

    API試してみた

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -45,10 +45,12 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
-	const  $ = __webpack_require__(1);
-	const  block = $('#block');
-	const  scalingButton = $('#scaling-button');
-	const  movingButton = $('#moving-button');
+	const $ = __webpack_require__(1);
+	const block = $('#block');
+	const scalingButton = $('#scaling-button');
+	const movingButton = $('#moving-button');
+	const hideButton = $('#hide-button');
+	const showButton = $('#show-button');
 
 	scalingButton.click(() => {
 	  block.animate({ width: '200pt', height: '200pt' }, 2000);
@@ -59,6 +61,14 @@
 	  block.animate({ marginLeft: '500px', width: '20pt', height: '20pt' }, 1000);
 	  block.animate({ margintop: '400px', width: '300pt', height: '300pt' }, 2000);
 	  block.animate({ marginLeft: '20px', margintop: '200px', width: '100pt', height: '100pt' }, 3000);
+	});
+
+	hideButton.click(() => {
+	  block.hide();
+	});
+
+	showButton.click(() => {
+	  block.show( 2000 );
 	});
 
 
