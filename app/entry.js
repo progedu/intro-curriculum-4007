bb7f2d73fdbb258e19448629313974cec8044974
Hideki Matsumoto <hdmatsum@gmail.com>
2017-11-11 07:44:29 +0000

    移動ボタンを追加

--- app/entry.js
+++ app/entry.js
@@ -1,9 +1,20 @@
 'use strict';
-const  $ = require('jquery');
-const  block = $('#block');
-const  scalingButton = $('#scaling-button');
+const $ = require('jquery');
+const block = $('#block');
+const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
+const colorButton = $('#color-button');
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+movingButton.click(() => {
+  block.animate({ marginLeft: '500pt' }, 500);
+  block.animate({ marginLeft: '20pt' }, 500);
+});
+
+colorButton.click(() => {
+  block.css( 'backgroundColor','#AA0000' );
 });
