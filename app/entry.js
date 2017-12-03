031ce4f789078845b20be9f14d66b3bc351b774e
Kosaburo Kaneko <33095006+KosaburoKaneko@users.noreply.github.com>
2017-12-03 18:25:50 +0100

    移動アニメーションの実装

--- app/entry.js
+++ app/entry.js
@@ -7,3 +7,8 @@ scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
+
+movingButton.click(() => {
+  block.animate({ 'marginLeft': '500px' }, 2000);
+  block.animate({ 'marginLeft': '20px' }, 2000);
+});
