b87d4d6cadcf243a583d3db5ffcb21de8f566852
208-209 <208_209@outlook.jp>
2018-01-15 11:18:56 +0000

    横移動のアニメーションを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   context: __dirname + '/app',
-  entry: './entry',
+  entry: ['./entry', './moving'],
   output: {
     path: __dirname + '/public/javascripts',
     filename: 'bundle.js'
