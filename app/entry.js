d2272070974ec3a0b8425570247a67cfba29ee8e
acua98dh <vbnsibc@yahoo.co.jp>
2018-01-30 11:04:25 +0000

    移動ボタンの実装 +他2ボタン実装

--- app/entry.js
+++ app/entry.js
@@ -2,8 +2,24 @@
 const  $ = require('jquery');
 const  block = $('#block');
 const  scalingButton = $('#scaling-button');
+const  movingButton = $('#moving-button');
+const  toCircleButton = $('#circle-button');
+const  toTransparentButton = $('#transparent-button');
+
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+movingButton.click(() => {
+  block.animate({ 'marginLeft': '500px' },500);
+  block.animate({ 'marginLeft': '20px' },1000);
+});
+toCircleButton.click(() => {
+  block.animate({ 'border-radius': '50pt' },2000);
+  block.animate({ 'border-radius': '0pt' },2000);
+});
+toTransparentButton.click(() => {
+  block.animate({ 'opacity': '0' },1000);
+  block.animate({ 'opacity': '1' },1000);
 });
