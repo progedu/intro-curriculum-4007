6f0c74d460e90aac0e12e3343b4cb6db70e2ce89
soundring <soundringing@yahoo.co.jp>
2018-02-07 12:19:38 +0000

    移動ボタンが0.5 秒間 かけて右に移動し、その後 1 秒かけて戻ってくる

--- app/entry.js
+++ app/entry.js
@@ -1,9 +1,15 @@
 'use strict';
-const  $ = require('jquery');
-const  block = $('#block');
-const  scalingButton = $('#scaling-button');
+const $ = require('jquery');
+const block = $('#block');
+const scalingButton = $('#scaling-button');
+const movingbutton = $('#moving-button');
 
 scalingButton.click(() => {
-  block.animate({ width: '200pt', height: '200pt' }, 2000);
+  block.animate({ width: '1000pt', height: '100pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+movingbutton.mouseover(() => {
+  movingbutton.animate({ 'marginLeft': '500px' }, 500);
+  movingbutton.animate({ 'marginLeft': '0px' }, 1000);
 });
