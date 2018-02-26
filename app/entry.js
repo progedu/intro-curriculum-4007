fbca7eb95466b78305b15d295e7ddfec52558f5d
Kensuke Takeda <takebbb1@gmail.com>
2018-02-26 04:05:40 +0000

    jQueryでアニメーション

--- app/entry.js
+++ app/entry.js
@@ -7,3 +7,14 @@ scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
+
+const  movingButton = $('#moving-button');
+
+movingButton.click(() => {
+  block.animate({ width: '20pt'}, 500);
+  block.animate({ width: '100pt'}, 50);
+  block.animate({'marginLeft': '500px' }, 200);
+  block.animate({'marginLeft': '20px'}, 200);
+  block.animate({ width: '20pt'}, 80);
+  block.animate({ width: '100pt'}, 500);
+});
