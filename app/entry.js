0d016427964481b06d924e58585b0e7e0e0c944f
4bntw <4bntw.g@gmail.com>
2018-03-28 11:08:50 +0000

    0.5秒かけて右に移動し、１秒かけて戻る動きを実装

--- app/entry.js
+++ app/entry.js
@@ -2,8 +2,13 @@
 const  $ = require('jquery');
 const  block = $('#block');
 const  scalingButton = $('#scaling-button');
+const  movingBtn = $('#moving-btn');
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+movingBtn.on('click', () => {
+  block.animate({'marginLeft': '500px'}, 500).animate({'marginLeft':'20px'}, 1000);
 });
