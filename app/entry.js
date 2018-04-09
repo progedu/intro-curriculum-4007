64eeef40e9d5064e72252c97c32fd3ff6ae20ed9
Naoto Miyai <miyaina@h9.dion.ne.jp>
2018-04-09 08:05:42 +0000

    移動ボタンを追加

--- app/entry.js
+++ app/entry.js
@@ -2,8 +2,14 @@
 const  $ = require('jquery');
 const  block = $('#block');
 const  scalingButton = $('#scaling-button');
+const movingaButton = $('#moving-button');
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+movingaButton.click(() => {
+  block.animate({ 'marginLeft' : '500pt'}, 500);
+  block.animate({ 'marginLeft' : '20pt'}, 1000);
 });
