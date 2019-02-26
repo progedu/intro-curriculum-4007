9932ae7ec30dd6d7ff12c956dbcd51e1f61ec2fd
Nada58 <mathtikoa@gmail.com>
2019-02-26 04:22:45 +0000

    アニメーションを追加

--- app/entry.js
+++ app/entry.js
@@ -2,8 +2,23 @@
 import $ from 'jquery';
 const block = $('#block');
 const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
+const round = $('#round');
+const opacity = $('#opacity');
 
 scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
+movingButton.click(() => {
+  block.animate({ 'marginLeft': '500px' }, 500);
+  block.animate({ 'marginLeft': '20px' }, 1000);
+});
+round.click(() => {
+  block.animate({borderRadius: '50%'}, 2000);
+  block.animate({borderRadius: '0%'}, 1000);
+})
+opacity.click(() => {
+  block.animate({ 'opacity': 0},1000);
+  block.animate({ 'opacity': 1},1000);
+})
