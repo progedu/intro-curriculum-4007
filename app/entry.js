553bde4f13a8d10b33e201bc92a9ea1660d8d405
Hatoremi <hatoremi@gmail.com>
2020-01-14 20:33:11 +0900

    四角形にランダムな移動を追加

--- app/entry.js
+++ app/entry.js
@@ -2,8 +2,22 @@
 import $ from 'jquery';
 const block = $('#block');
 const scalingButton = $('#scaling-button');
+const movingButton = $('#moving-button');
 
 scalingButton.click(() => {
-  block.animate({ width: '200pt', height: '200pt' }, 2000);
-  block.animate({ width: '100pt', height: '100pt' }, 2000);
+  block.animate({ width: '200px', height: '200px' }, 2000);
+  block.animate({ width: '100px', height: '100px' }, 2000);
 });
+
+movingButton.click(() => {
+  block.animate({ marginLeft: getRandomInt(50, 600) +'px'}, getRandomInt(100, 500));
+  block.animate({ marginTop: getRandomInt(100, 700) +'px'}, getRandomInt(100, 800));
+  block.animate({ marginLeft: '20px'}, getRandomInt(100, 800));
+  block.animate({ marginTop: '20px'}, getRandomInt(100, 2000));
+});
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
