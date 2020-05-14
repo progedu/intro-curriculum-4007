b91a8897c54c30c4fc86f37226a613da10b9980c
Yoshiki Shiota <rals.shio.y@gmail.com>
2020-05-14 20:34:25 +0900

    [fix]移動するボタンの追加

--- app/entry.js
+++ app/entry.js
@@ -7,3 +7,11 @@ scalingButton.click(() => {
   block.animate({ width: '200pt', height: '200pt' }, 2000);
   block.animate({ width: '100pt', height: '100pt' }, 2000);
 });
+
+// 移動ボタンの取得
+const movingButton = $('#moving-button');
+// クリック関数
+movingButton.click(() => {
+  block.animate({ marginLeft: '500pt' }, 500);
+  block.animate({ marginLeft: '20pt' }, 1000);
+});
