82ae65cc204b2d55de7c9b9d9c84e15af1206870
haruppe-tonothyme <haruppe0413@icloud.com>
2020-07-12 12:08:57 +0900

    移動ボタンの実装

--- app/entry.js
+++ app/entry.js
@@ -4,6 +4,13 @@ const block = $('#block');
 const scalingButton = $('#scaling-button');
 
 scalingButton.click(() => {
-  block.animate({ width: '200pt', height: '200pt' }, 2000);
-  block.animate({ width: '100pt', height: '100pt' }, 2000);
+    block.animate({ width: '200pt', height: '200pt' }, 2000);
+    block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
+
+const movingButton = $('#moving-button');
+
+movingButton.click(() => {
+    block.animate({ 'marginLeft': '500px' }, 100);
+    block.animate({ 'marginLeft': '20px' }, 50);
 });
