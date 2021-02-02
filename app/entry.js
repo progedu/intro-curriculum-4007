4c97edfafef7672bef2d0f846287c66a20276e39
ihwka <dbgt012002@gmail.com>
2021-02-02 14:54:55 +0900

    「移動」ボタンを実装(右への移動時間を修正)

--- app/entry.js
+++ app/entry.js
@@ -4,6 +4,6 @@ const block = $('#block');
 const movingButton = $('#moving-button');
 
 movingButton.click(() => {
-  block.animate({ 'marginLeft': '500px' }, 1000);
+  block.animate({ 'marginLeft': '500px' }, 500);
   block.animate({ 'marginLeft': '20px' }, 1000);
 });
--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -100,7 +100,7 @@ var movingButton = jquery__WEBPACK_IMPORTED_MODULE_0___default()('#moving-button
 movingButton.click(function () {
   block.animate({
     'marginLeft': '500px'
-  }, 1000);
+  }, 500);
   block.animate({
     'marginLeft': '20px'
   }, 1000);
